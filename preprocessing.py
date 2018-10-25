ca75f51b302caf62caff29bf858edc00b1134fdf
Roger Granada <roger.leitzke@gmail.com>
2018-10-25 17:34:43 -0300

    Added remotion of classes that are not in ground truth

--- preprocessing.py
+++ preprocessing.py
@@ -16,6 +16,20 @@ are previously converted to JSON format in the form:
     ...
   ]
 } 
+
+# Pipeline of Pre-Processing:
+  - Remove classes from faster and leannet that do not belong to ground truth
+     $ python preprocessing.py -o faster_gt.json -m check_classes -g GT.json faster.json 
+     $ python preprocessing.py -o leannet_gt.json -m check_classes -g GT.json leannet.json 
+
+  - Apply threshold on faster_cor.json and leannet_cor.json [set to 0.5]
+     $ python preprocessing.py -o leannet_0.5.json -m apply_threshold leannet_gt.json
+     $ python preprocessing.py -o faster_0.5.json -m apply_threshold faster_gt.json
+
+  - Remove all images that do not appear the three: faster.json, leannet.json and GT.json
+     $ python preprocessing.py -o leannet_tmp.json -m align_files -g GT.json leannet_0.5.json
+     $ python preprocessing.py -o faster_0.5f.json -m align_files -g leannet_tmp.json faster_0.5.json
+     $ python preprocessing.py -o leannet_0.5f.json -m align_files -g faster_0.5f.json leannet_tmp.json
 """
 import logging
 logger = logging.getLogger(__name__)
@@ -37,9 +51,17 @@ def check_file(input):
 
 
 def save_json(output, dic):
+    # Ensure that there is not any image without objects
+    dout = {}
+    for image in dic:
+        if dic[image]:
+            dout[image] = dic[image]
+        else:
+            logger.info('Image without bounding box: %s' % image)
+
     logger.info('Saving file %s' % output)
     with open(output, 'w') as outfile:
-        json.dump(dic, outfile)
+        json.dump(dout, outfile)
 
 
 def read_json(input):
@@ -93,6 +115,30 @@ def align_files(file_predict, file_ground, output):
     logger.info('Total of aligned images: %d' % aligned)
 
 
+def check_classes(file_predict, file_ground, output):
+    """
+    Ensure that predicted labels correspond to the ground truth
+    """
+    dground = read_json(file_ground)
+    dg = {}
+    for image in dground:
+        for obj in dground[image]:
+            dg[obj[0]] = ''
+
+    dic = {}
+    dpredict = read_json(file_predict)
+    for image in dpredict:
+        for obj in dpredict[image]:
+            if dg.has_key(obj[0]):
+                if dic.has_key(image):
+                    dic[image].append(obj)
+                else:
+                    dic[image] = [obj]
+            else:
+                logger.info('Discarding bounding box of class: %s' % obj[0])
+    save_json(output, dic)
+
+
 def main(file_predict, file_ground, output, mode, threshold):
     if not output:
         dirin = dirname(file_predict)
@@ -102,6 +148,8 @@ def main(file_predict, file_ground, output, mode, threshold):
         align_files(file_ground, file_predict, output)
     elif mode.lower() == 'apply_threshold':
         apply_threshold(file_predict, output, threshold)
+    elif mode.lower() == 'check_classes':
+        check_classes(file_predict, file_ground, output)
     else:
         logger.error('Mode for pre-processing is not correct: %s' % mode)
 
@@ -112,7 +160,7 @@ if __name__ == "__main__":
     parser.add_argument('-g', '--groundtruth', help='File containing ground truth for all images', default=None)
     parser.add_argument('-o', '--output', help='File to save the generated json file', default=None)
     parser.add_argument('-t', '--threshold', help='Apply threshold on predicted scores', default=0.5)
-    parser.add_argument('-m', '--mode', help='Mode of pre-processing (align_files|apply_threshold)', default='align_files')
+    parser.add_argument('-m', '--mode', help='Mode of pre-processing (align_files|apply_threshold|check_classes)', default='align_files')
     args = parser.parse_args()
 
     main(args.predicted, args.groundtruth, args.output, args.mode, args.threshold)
